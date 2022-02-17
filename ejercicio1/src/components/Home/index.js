90adc0c4b3bea278ad1494ba1048596563193983
dsoriam <dsoriam@gmail.com>
2022-02-17 17:16:40 -0300

    Agrego componentes Nav y Home

---
+++ ejercicio1/src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './index.css';
+
+
+function Home (){
+    return (
+        <div>
+            hhhahshahsdhashdasdbasbdbaskdbaks asdjhajksdhkajshd ashdkjahsjkdhjahds ajsdhkasjhd
+    </div>
+
+    );
+}
+
+export default Home;
---
+++ ejercicio1/src/components/Navv/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './index.css';
+
+
+function Navv (){
+    return (
+    <p>Holaaaaaa</p> 
+    );
+}
+
+
+export default Navv;
