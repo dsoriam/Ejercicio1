9d26f83fba8550941a303068e6624b5f48b09d31
dsoriam <dsoriam@gmail.com>
2022-02-21 17:03:12 -0300

    Se cargan fotos

--- ejercicio1/src/components/Home/index.js
+++ ejercicio1/src/components/Home/index.js
@@ -1,15 +1,25 @@
 import React from "react";
 import './index.css';
-
+import yoo from './yoo.png';
 
 function Home (){
     return (
         <>
             <h2>Daniel Soria - TI </h2>
-    <       p>Soy técnico en Sistemas Operativos y Redes, actualmente me encuentro trabajando y estudiando para poder ser programador</p> 
-    </>
+            <p> Tengo 32 años. Soy técnico en Sistemas Operativos y Redes, 
+                actualmente me encuentro trabajando y estudiando para poder ser programador.
+                Vivo en Fray bentos</p> 
+            
+            <img src={yoo} alt="mi foto"/>
+            <button> Ver CV </button>
 
-    );
+            function BotonVer(){
+                return ();
+                
+            } ;
+        </>
+
+        );
 }
 
 export default Home;
